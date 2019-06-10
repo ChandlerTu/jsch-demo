b239beeca12bd380f177b9af49f3bdea7b11b0a9
tuchangle <tuchangle@163.com>
2019-06-10 09:34:40 +0800

    新建 Spring Boot 项目。

---
+++ src/test/java/com/guolisecurity/jsch/JschDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guolisecurity.jsch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JschDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
